17bfe724702ffc1aa77bc607992d758dfecbb25e
hinameplays <heinrich-karow@gmx.de>
2021-02-11 09:57:00 +0100

    updated functionality

--- list/Aufgabenliste 0/Liste.java
+++ list/Aufgabenliste 0/Liste.java
@@ -1,31 +1,64 @@
+import java.util.NoSuchElementException;
+
 class Liste<T> {
     Listenelement anfang;
     Listenelement ende;
     int length = getLength();
     
-    void anhaengen(T o) {
-        Listenelement neuesElement = new Listenelement(o);
+    void add(T o) {
+        Listenelement neuesElement = new Listenelement(o); // neues Listenelement aus Objekt erzeugt -> Listenelement()
         if(anfang == null) {
+            // Sonderfall: Liste noch leer
             anfang = neuesElement;
             ende = neuesElement;
         } else {
             ende.next = neuesElement;
             ende = neuesElement;
         }
-        length = getLength();
+        length = getLength(); // neues berechnen von length
     }
 
-    T getEnde () {
+    void remove(Listenelement e) {
+        int a = getPosition(e);
+        if (a>1) {
+            get(a-1).next = e.next;
+        } else {
+            anfang = e.next; //wenn e erstes Element ist, Nachfolger zum ersten
+        }
+        e.next = null;  //e loslösen
+        length = getLength(); // neues berechnen von length
+    }
+
+    T getEndeContent () {
         // Gib nicht das Listenelement, sondern dessen content zurück
         return ende.content;
     }
 
+    int getPosition(Listenelement e) throws NoSuchElementException {
+        Listenelement a=anfang;
+
+        if (e==null) {
+            throw new NoSuchElementException("Element given is null.");
+        }
+
+        for (int j=1; j<=length; j++) {
+            if (a == e) {
+                return j;
+            }
+            a=a.next;
+        }
+
+        throw new NoSuchElementException("Element given not part of list.");
+    }
+
     Listenelement get (int i) {
+        //Sonderfälle von letztem Element und ungültigem i puffern
         if(i>length||i<1) {
             return null;
         } else if (i==length) {
             return ende;
         }
+
         Listenelement a = anfang;
         for (int h = 1; h<i; h++) {
             a = a.next;
@@ -36,6 +69,7 @@ class Liste<T> {
     int getLength () {
         Listenelement a = anfang;
         int i=0;
+        //loop über alle Elemente
         while (a!=null) {
             i++;
             a = a.next;
@@ -43,6 +77,7 @@ class Liste<T> {
         return i;
     }
 
+    //interne Listenelement-Definition
     class Listenelement {
         Listenelement next;
         T content;
--- list/Aufgabenliste 0/Testprogramm.java
+++ list/Aufgabenliste 0/Testprogramm.java
@@ -2,6 +2,7 @@ class Testprogramm
 {
     public static void testprogramm()
     {
+        // Aufgabe in testprogramm definiert, da sonst unnötig
         class Aufgabe {
             String beschreibung;
         
@@ -13,11 +14,11 @@ class Testprogramm
         Liste<Aufgabe> liste = new Liste<Aufgabe>();
         // seit Java 7 auch möglich:
         // Liste<Aufgabe> liste = new Liste<>();
-        liste.anhaengen(new Aufgabe("Aufstehen"));
-        liste.anhaengen(new Aufgabe("Frühstücken"));
-        liste.anhaengen(new Aufgabe("In die Schule gehen"));
-        // Typecast ist nicht mehr notwendig
-        Aufgabe a = liste.getEnde();
+        liste.add(new Aufgabe("Aufstehen"));
+        liste.add(new Aufgabe("Frühstücken"));
+        liste.add(new Aufgabe("In die Schule gehen"));
+        // Typecast ist nicht mehr notwendig; letztes Element ausgeben
+        Aufgabe a = liste.getEndeContent();
         System.out.println(a.beschreibung);
         // Geht nicht mehr
         // liste.anhaengen(new String());
@@ -25,8 +26,11 @@ class Testprogramm
         System.out.println(liste.get(1).content.beschreibung);
         System.out.println(liste.get(2).content.beschreibung);
         System.out.println(liste.get(3).content.beschreibung);
-        System.out.println(liste.get(4).content.beschreibung);
-        System.out.println(liste.get(0).content.beschreibung);
+        //System.out.println(liste.get(4).content.beschreibung);
+        //System.out.println(liste.get(0).content.beschreibung);
+        System.out.println(liste.getPosition(liste.get(3)));
+        liste.remove(liste.get(2));
+        System.out.println(liste.get(2).content.beschreibung);
     }
 
     
